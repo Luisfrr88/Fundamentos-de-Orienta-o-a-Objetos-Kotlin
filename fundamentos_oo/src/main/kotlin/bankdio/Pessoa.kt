311b24afd4d02060a19079cd348c63ea5fbeadf8
Luis <luisfrr88@gmail.com>
2022-01-06 11:51:09 -0300

    Update to talk about members

--- fundamentos_oo/src/main/kotlin/bankdio/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/bankdio/Pessoa.kt
@@ -2,15 +2,18 @@ package bankdio
 
 class Pessoa {
     var nome: String = "Luis"
+
     var cpf: String = "000.000.001.02"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val luis = Pessoa()
 
-    println(luis.nome)
-    println("--------------------------")
-    println(luis.cpf)
+ println(luis.pessoaInfo())
 
 }
