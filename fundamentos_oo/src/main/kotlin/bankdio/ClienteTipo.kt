b4eee6e902a7e25fd3e3faa0a461ba89a9b164b2
Luis <luisfrr88@gmail.com>
2022-01-09 12:25:19 -0300

    Create enum client type and test to it!

--- fundamentos_oo/src/main/kotlin/bankdio/ClienteTipo.kt
+++ fundamentos_oo/src/main/kotlin/bankdio/ClienteTipo.kt
@@ -1,6 +1,7 @@
 package bankdio
 
-enum class ClienteTipo(descricao: String) {
+enum class ClienteTipo(val descricao: String) {
+
     PF("Pessoa Fisica"),
     PJ("Pessoa Juridica")
 }
--- fundamentos_oo/src/main/kotlin/bankdio/Teste/TesteTipoEnum.kt
+++ fundamentos_oo/src/main/kotlin/bankdio/Teste/TesteTipoEnum.kt
@@ -4,12 +4,20 @@ import bankdio.ClienteTipo
 
 fun main() {
     ClienteTipo.values().forEach { elemento ->
-        println("${elemento.name} $elemento")
+        println("${elemento.name} - ${elemento.descricao}")
     }
 
     ClienteTipo.values().forEach {
-        println("${it.name} $it")
+        println("${it.name} - ${it.descricao}")
     }
+    val pf = ClienteTipo.PF
+
+    println("${pf.name} - ${pf.descricao}" )
+
+    val pj = ClienteTipo.PJ
+
+    println("${pj.name} - ${pj.descricao}")
+
 
 }
 
